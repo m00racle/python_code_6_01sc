4d75f88ecd31ff568e2be04440770b56b68f1f3f
mooracle <yanuar.prakosa@gmail.com>
2022-11-21 20:38:19 +0700

    lab work 1 Object Oriented Programming

--- Lec_1/designLab01Work.py
+++ Lec_1/designLab01Work.py
@@ -9,17 +9,100 @@
 
 def fib(n):
     # Delete the pass statement below and insert your own code
-    pass
+    # for safety unable to process negative arguments
+    if n < 0 : raise ValueError("argument cannot be negative")
+    if n == 0 : return 0
+    if n == 1 : return 1
+    else : return fib(n-1) + fib(n-2)
 
 #-----------------------------------------------------------------------------
 
-class V2:
+class V2(object):
     # Delete the pass statement below and insert your own code
-    pass
+    def __init__(self, x : float, y : float) -> None:
+        self.x = float(x)
+        self.y = float(y)
 
+    def getX(self) -> float:
+        return self.x
+
+    def getY(self) -> float:
+        return self.y
+
+    def __str__(self):
+       
+        return 'V2[' + str(self.x) + ", " + str(self.y) + ']'
+
+    def add(self, v):
+        return V2(self.x + v.getX(), self.y + v.getY())
+
+    def mul(self, v):
+        return V2(self.x * v, self.y * v)
+
+    def __add__(self, v):
+        return self.add(v)
+
+    def __mul__(self, v):
+        return self.mul(v)
+
+    
+    
 #-----------------------------------------------------------------------------
 
 class Polynomial:
     # Delete the pass statement below and insert your own code
-    pass
+    def __init__(self, coeffs : list) -> None:
+        self.order = len(coeffs) - 1
+        self.coeffs = coeffs
+    
+    def __str__(self) -> str:
+        text = ''
+        order = self.order
+        for coeff in self.coeffs:
+            if order > 1 : 
+                text += f'{float(coeff):.3f}' + ' z**' + str(order) + ' + '
+            elif order == 1:
+                text += f'{float(coeff):.3f}' + ' z + '
+            else:
+                text += f'{float(coeff):.3f}'
+            
+            order -= 1
+        return text
+    
+    def add(self, p2 ) :
+        co_ex = []
+        coeffs1 = self.coeffs.copy()
+        coeffs2 = p2.coeffs.copy()
+        # I need to copy the coeffs to maintain the original list while I preseve this :
 
+        while len(coeffs1) > 0 and len(coeffs2) > 0:
+            co_ex.insert(0, coeffs1[-1] + coeffs2[-1])
+            coeffs2.pop()
+            coeffs1.pop()
+        
+        # check which coeffs is still has element in it :
+        # NOTE: the coeefs here list added to list thus use operator + instead of extend
+        if len(coeffs1) > 0 : return Polynomial(coeffs1 + co_ex)
+        return Polynomial(coeffs2 + co_ex)
+
+    def __add__(self, po):
+        return self.add(po)
+
+    def __call__(self, x) -> float:
+        """  
+        return the result of function when passed an argument x
+        """
+        result = 0.0
+        order = self.order
+        for coeff in self.coeffs:
+            result += float(coeff) * float(x) ** (order)
+            order -= 1
+        return result
+
+def runPoly():
+    p1 = Polynomial([1,2,3])
+    p2 = Polynomial([100,200])
+    print(p1.add(p2))
+
+if __name__ == '__main__':
+    runPoly()
---
+++ test/test_designLab01Work.py
@@ -0,0 +1,89 @@
+import os
+import sys
+
+# set the code and test dir
+test_dir = os.path.dirname(__file__)
+code_dir = os.path.normpath(test_dir + "/../Lec_1")
+
+sys.path.append(code_dir)
+
+import unittest
+
+# import the source code that will be tested
+import designLab01Work as src
+
+class Test_designLab01Work(unittest.TestCase):
+    """  
+    test suite for the design Lab 01
+    all labwork must pass all test here before submitted.
+    """
+
+    def setUp(self) -> None:
+        self.v = src.V2(1.0, 2.0)
+        self.b = src.V2(2.2, 3.3)
+        self.p1 = src.Polynomial([1,2,3])
+        self.p2 = src.Polynomial([100,200])
+
+    def test_fib_0_returns_0(self):
+        self.assertEqual(src.fib(0), 0, "fib(0) FAILED to return 0")
+    
+    def test_fib_1_returns_1(self):
+        self.assertEqual(src.fib(1), 1, "fib(1) FAILED to returns 1")
+
+    def test_fib_6_returns_8(self):
+        self.assertEqual(src.fib(6), 8, "fib(6) FAILED to returns 6")
+    
+    def test_negative_argument_raise_value_error(self):
+        with self.assertRaises(ValueError) as e:
+            src.fib(-2)
+        
+        self.assertTrue("argument cannot be negative" in e.exception.args)
+    
+    # test for Object Oriented Practice
+    def test_print_V2_returns_class_name_and_list(self):
+        self.assertEqual(str(src.V2(1.1, 2.2)), "V2[1.1, 2.2]", "V2 __str__ function FAILED")
+    
+    def test_get_X_retuns_the_first_vector_element(self):
+        self.assertEqual(self.v.getX(), 1.0, "getX FAILED")
+    
+    def test_get_Y_return_the_second_vector_element(self):
+        self.assertEqual(self.v.getY(), 2.0, "getY FAILED")
+    
+    def test_add_v_return_addition_with_vector_in_the_argument(self):
+        self.assertEqual(str(self.v.add(self.b)), "V2[3.2, 5.3]", "add function FAILED")
+
+    def test_mul_v_return_vector_to_scalar_multiplication(self):
+        self.assertEqual(str(self.v.mul(2)), "V2[2.0, 4.0]", "mul function FAILED")
+
+    def test_add_and_mul_return_scalar_mult_after_vector_add(self):
+        self.assertEqual(str(self.v.add(self.b).mul(-1)), "V2[-3.2, -5.3]", "combination functions add and mul FAILED")
+
+    def test_addition_operator_overloading_between_two_vectors(self):
+        self.assertEqual(str(src.V2(1.1, 2.2) + src.V2(3.0, 4.0)), "V2[4.1, 6.2]", "addition overloading FAILED")
+    
+    def test_multiplication_operator_overloadin_between_vector_and_scalar(self):
+        self.assertEqual(str(self.b * (-1)), "V2[-2.2, -3.3]", "multiplication operator overloadinf FAILED")
+
+    # test class Polynomial
+    def test_print_polynomials(self):
+        self.assertEqual(str(self.p1), "1.000 z**2 + 2.000 z + 3.000", "__str__ Polynomial FAILED")
+    
+    def test_add_function_pass_other_poly_as_argument(self):
+        self.assertEqual(str(self.p1.add(self.p2)), "1.000 z**2 + 102.000 z + 203.000", "add function Polynomial FAILED")
+
+    def test_add_function_longer_poly_as_argument(self):
+        self.assertEqual(str(self.p2.add(self.p1)), "1.000 z**2 + 102.000 z + 203.000", "add function Polynomial FAILED")
+
+    def test_pass_argument_to_polynomial(self):
+        self.assertEqual(self.p1(1), 6.0, "__call__ FAILED")
+
+    def test_pass_negative_argument_to_polynomial(self):
+        self.assertEqual(self.p1(-1), 2.0, "__call__ FAILED")
+
+    def test_operator_overloading_addition(self):
+        self.assertEqual(str(self.p1 + self.p2), "1.000 z**2 + 102.000 z + 203.000", "__add__ function Polynomial FAILED")
+    
+    # additional requirement for higher order polynomial but with zero coeffs
+    def test_zero_coeff_for_polynomial(self):
+        self.assertEqual(str(src.Polynomial([3,0,0,0,0])), "3.000 z**4", "zero coeff handler FAILED")
+    
