334642ba7bea652e2605009d01d21cc08777b1a3
mooracle <yanuar.prakosa@gmail.com>
2022-11-23 16:58:17 +0700

    finish the prblem wk 1.3.5

--- Lec_1/polyreps.py
+++ Lec_1/polyreps.py
@@ -23,6 +23,14 @@ def part1():
     print((lab.Polynomial([3,0,2,4]) * lab.Polynomial([2,0])).getReps())
     print(f"prove: {lab.Polynomial([3,0,2,4]) * lab.Polynomial([2,0])}")
 
+    print("\n6. Enter the sequence of coefficients for the polynomial that is the sum of the two polynomials above (3x**3 + 2x - 4) + (7):")
+    print((lab.Polynomial([3,0,2,4]) * lab.Polynomial([7])).getReps())
+    print(f"prove: {lab.Polynomial([3,0,2,4]) * lab.Polynomial([7])}")
+
+    print("\n Enter the sequence of coefficients for the polynomial that is the sum of the previous two resulting polynomials. ")
+    print((lab.Polynomial([3,0,2,4]) * lab.Polynomial([2,0]) + lab.Polynomial([3,0,2,4]) * lab.Polynomial([7])).getReps())
+    print(f"prove: {(lab.Polynomial([3,0,2,4]) * lab.Polynomial([2,0])) + (lab.Polynomial([3,0,2,4]) * lab.Polynomial([7]))}")
+
 
 if __name__ == '__main__':
     part1()
