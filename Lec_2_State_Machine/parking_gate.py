9cfa3faa1914e914db4554984519e0a036fa3e42
m00racle <yanuar.prakosa@gmail.com>
2022-12-04 16:47:03 +0700

    modify getNextValue include nextState and output
    NOTE: still using super.getNextValues(...) to handle the rest
    This includes handle the exception still uses the super function

--- Lec_2_State_Machine/parking_gate.py
+++ Lec_2_State_Machine/parking_gate.py
@@ -10,45 +10,38 @@ class FreeGate(SM):
         free gate initial state always 'waiting'
         """
         super().__init__('waiting')
-
-    def generateOuput(self, nextState, inp):
-        # nextState = self.generateState(state, inp)
-        if nextState == 'raising' :
-            return 'lift'
-        elif nextState == 'lowering' :
-            return 'drop'
-        else :
-            return 'nop'
     
-    def generateState(self, state, inp):
-        gatePos, carIn, carOut = inp
-         # detect run over (the most bold one:  just straight runover gate)
-        if state == 'waiting' and carOut:
-            raise RunOverViolation('Run Over')
-        if state == 'waiting' and carIn:
-            return 'raising'
-        elif state == 'raising' and gatePos == 'top':
-            return 'raised'
-        # detect run over:
-        elif state == 'raising' and gatePos == 'bottom' and carOut: 
-            raise RunOverViolation("Run Over")
-        # detect too soon violation:
-        elif state == 'raising' and gatePos == 'middle' and carOut:
-            raise TooSoonViolation('Too Soon')
-        elif state == 'raised' and carOut:
-            return 'lowering'
-        # detecting too soon violation while drop
-        elif state == 'lowering' and gatePos == 'middle' and carOut:
-            raise TooSoonViolation('Too Soon')
-        elif state == 'lowering' and gatePos == 'bottom' :
-            return 'waiting'
-        else:
-            return state
     
     def getNextValues(self, state, inp, fn=..., fo=..., efn=None, efo=None) -> tuple:
-        nextState = self.generateState(state, inp)
+        gatePos, carIn, carOut = inp
+        # detect violations
+        if state == 'waiting' and carOut : raise RunOverViolation('halt')
+        if state == 'raising' and gatePos == 'bottom' and carOut : raise RunOverViolation('halt')
+        if state == 'raising' and gatePos == 'middle' and carOut : raise TooSoonViolation('halt')
+        if state == 'lowering' and gatePos == 'middle' and carOut : raise TooSoonViolation('halt')
+
+        # determine next state:
+        if state == 'waiting' and carIn:
+            nextState = 'raising'
+        elif state == 'raising' and gatePos == 'top':
+            nextState = 'raised'
+        elif state == 'raised' and carOut:
+            nextState = 'lowering'
+        elif state == 'lowering' and gatePos == 'bottom' :
+            nextState = 'waiting'
+        else:
+            nextState = state
+        
+        # determine the ouput:
+        if nextState == 'raising' :
+            output = 'lift'
+        elif nextState == 'lowering' :
+            output = 'drop'
+        else :
+            output = 'nop'
+
         fn = lambda s,i : nextState
-        fo = lambda s,i : self.generateOuput(nextState, inp)
+        fo = lambda s,i : output
         return super().getNextValues(state, inp, fn, fo, efn, efo)
 
 class RunOverViolation(Exception):
