efd44d0806edb607742e6177e09b3613fdcb8123
mooracle <yanuar.prakosa@gmail.com>
2022-12-05 14:21:23 +0700

    fix free gate erorr handling
    The raise is in the local getNextValue  NOT in the superclass
    
    Thus the handling must be done locally
    
    I will try another approach which is enable super class error handling

--- Lec_2_State_Machine/parking_gate.py
+++ Lec_2_State_Machine/parking_gate.py
@@ -12,38 +12,43 @@ class FreeGate(SM):
         super().__init__('waiting')
 
     def getNextValues(self, state, inp, **kwargs) -> tuple:
-        gatePos, carIn, carOut = inp
-        # detect violations
-        if state == 'waiting' and carOut : raise RunOverViolation('ALERT! run off')
-        if state == 'raising' and gatePos == 'bottom' and carOut : raise RunOverViolation('ALERT! run off')
-        if state == 'raising' and gatePos == 'middle' and carOut : raise TooSoonViolation('ALERT! too soon')
-        if state == 'lowering' and gatePos == 'middle' and carOut : raise TooSoonViolation('ALERT! too soon')
+        try:
+            gatePos, carIn, carOut = inp
+            # detect violations
+            if state == 'waiting' and carOut : raise RunOverViolation('ALERT! run off')
+            if state == 'raising' and gatePos == 'bottom' and carOut : raise RunOverViolation('ALERT! run off')
+            if state == 'raising' and gatePos == 'middle' and carOut : raise TooSoonViolation('ALERT! too soon')
+            if state == 'lowering' and gatePos == 'middle' and carOut : raise TooSoonViolation('ALERT! too soon')
 
-        # determine next state:
-        if state == 'waiting' and carIn:
-            nextState = 'raising'
-        elif state == 'raising' and gatePos == 'top':
-            nextState = 'raised'
-        elif state == 'raised' and carOut:
-            nextState = 'lowering'
-        elif state == 'lowering' and gatePos == 'bottom' :
-            nextState = 'waiting'
-        else:
-            nextState = state
+            # determine next state:
+            if state == 'waiting' and carIn:
+                nextState = 'raising'
+            elif state == 'raising' and gatePos == 'top':
+                nextState = 'raised'
+            elif state == 'raised' and carOut:
+                nextState = 'lowering'
+            elif state == 'lowering' and gatePos == 'bottom' :
+                nextState = 'waiting'
+            else:
+                nextState = state
 
-        # determine the ouput:
-        if nextState == 'raising' :
-            output = 'lift'
-        elif nextState == 'lowering' :
-            output = 'drop'
-        else :
-            output = 'nop'
-        
-        kwargs = {
-            'fn' : lambda s,i : nextState,
-            'fo' : lambda s,i : output
-        }
-        return super().getNextValues(state, inp, **kwargs)
+            # determine the ouput:
+            if nextState == 'raising' :
+                output = 'lift'
+            elif nextState == 'lowering' :
+                output = 'drop'
+            else :
+                output = 'nop'
+            
+            kwargs = {
+                'fn' : lambda s,i : nextState,
+                'fo' : lambda s,i : output
+            }
+            return super().getNextValues(state, inp, **kwargs)
+        except Exception as e:
+            # NOTE: because the raise exception is not inside the fn and fo then it means it can't be handled in superclass
+            # thus we need to handle exception here but I still can use the super foErr function
+            return (self.fnErr(state, inp, e), self.foErr(state, inp, e))
 
     def fnErr(self, state, inp, err):
         state = 'halt'
