496930c71e75d42688abade0b1e5f25e528d996e
mooracle <yanuar.prakosa@gmail.com>
2023-01-19 21:15:06 +0700

    preps for feedback test scenario

--- test/test_feedback_sm.py
+++ test/test_feedback_sm.py
@@ -11,18 +11,22 @@ Test page for Feedback combinator
 
 # import from file targeted for testing
 from feedback_sm import Feedback
+from cascade import Cascade, Increment, Delay
 
 class TestFeedback(unittest.TestCase):
     """  
     test scenario for Feedback class
     """
 
-    def proposed_test_increment_feedback_output(self):
+    def test_increment_feedback_output(self):
         """  
         Test the output only for the incrementer state machine implementing feedback
         NOTE: I need means to limit the run of the feedback to prevent infinite loop.
         """
-        self.fail("NO TEST")
+        # arrange
+        f = Feedback(Cascade(Increment(2), Delay(3)))
+        # assert
+        self.assertEqual(f.run(), [3,5,7,9,11,13,15,17,19,21])
 
     def proposed_test_increment_feedback_verbose(self):
         """  
