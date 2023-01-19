8b4a50c4ae7a2bc459c76f8287b47d6ca96ad995
mooracle <yanuar.prakosa@gmail.com>
2023-01-20 06:07:40 +0700

    first trial feedback failed test still out all None

--- Lec_2_State_Machine/feedback_sm.py
+++ Lec_2_State_Machine/feedback_sm.py
@@ -10,3 +10,12 @@ class Feedback(SM):
     class Feedback inherit SM class
     This is the model for feedback state machine
     """
+    def __init__(self, sm : SM) -> None:
+        self.sm = sm
+        self.startState = 0
+
+    def getNextValues(self, state, inp, **kwargs) -> tuple:
+        (ignore, o) = self.sm.getNextValues(state, 'undefined')
+        (newS, ignore) = self.sm.getNextValues(state, o)
+        return (newS, o)
+    
