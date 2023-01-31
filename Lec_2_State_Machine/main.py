cb44aee5757f3a1eb59a663e35d89472b7fbbe48
mooracle <yanuar.prakosa@gmail.com>
2023-01-31 18:12:29 +0700

    Lec_2 main add exercise 4.8 and 4.9

--- Lec_2_State_Machine/main.py
+++ Lec_2_State_Machine/main.py
@@ -1,4 +1,4 @@
-from state_machine import Delay, Increment, Adder, Wire
+from state_machine import Delay, Increment, Adder, Wire, Multiplier, Fixed
 from cascade import Cascade
 from feedback_sm import Feedback
 from parallel import Parallel
@@ -50,6 +50,22 @@ def main()->None:
     print(f'\nExercise 4.7: ')
     print(f'fib2 results: {fed2.run(verbose=True)}\n')
     print(f'fib2 results: {fed3.run(verbose=True)}\n')
+
+    # Exercise 4.8:
+    """  
+    Use feedback and a multiplier (analogous to Adder) to make a machine whose output doubles on every step.
+    """
+    mul1 = Feedback(Cascade(Parallel(Delay(1), Fixed(2)), Multiplier()))
+    print(f'\nExercise 4.8: ')
+    print(f'mul1 results: {mul1.run(verbose=True)}\n')
+
+    # Exercise 4.9:
+    """  
+    Use feedback and a multiplier (analogous to Adder) to make a machine whose output squares on every step.
+    """
+    mul2 = Feedback(Cascade(Parallel(Delay(2), Delay(2)), Multiplier()))
+    print(f'\nExercise 4.9: ')
+    print(f'mul2 results: {mul2.run(3, verbose=True)}\n')
 
 if __name__ == '__main__':
     main()
@@ -119,4 +135,53 @@ In: None Out: 21 Next State: ((21, (21, 13)), 0)
 In: None Out: 34 Next State: ((34, (34, 21)), 0)
 In: None Out: 55 Next State: ((55, (55, 34)), 0)
 fib1 results: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+Exercise 4.7:
+Start state: ((0, (1, 0)), 0)
+In: None Out: None Next State: ((0, (None, 1)), 0)        
+In: None Out: None Next State: ((0, (None, None)), 0)     
+In: None Out: None Next State: ((0, (None, None)), 0)     
+In: None Out: None Next State: ((0, (None, None)), 0)     
+In: None Out: None Next State: ((0, (None, None)), 0)     
+In: None Out: None Next State: ((0, (None, None)), 0)     
+In: None Out: None Next State: ((0, (None, None)), 0)     
+In: None Out: None Next State: ((0, (None, None)), 0)     
+In: None Out: None Next State: ((0, (None, None)), 0)     
+In: None Out: None Next State: ((0, (None, None)), 0)     
+fib2 results: [None, None, None, None, None, None, None, None, None, None]
+
+Start state: ((0, 1), 0)
+In: None Out: 1 Next State: ((1, 1), 0)
+In: None Out: 2 Next State: ((2, 2), 0)
+In: None Out: 4 Next State: ((4, 4), 0)
+In: None Out: 8 Next State: ((8, 8), 0)
+In: None Out: 16 Next State: ((16, 16), 0)
+In: None Out: 32 Next State: ((32, 32), 0)
+In: None Out: 64 Next State: ((64, 64), 0)
+In: None Out: 128 Next State: ((128, 128), 0)
+In: None Out: 256 Next State: ((256, 256), 0)
+In: None Out: 512 Next State: ((512, 512), 0)
+fib2 results: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]     
+
+
+Exercise 4.8:
+Start state: ((1, 0), 0)
+In: None Out: 2 Next State: ((2, 0), 0)
+In: None Out: 4 Next State: ((4, 0), 0)
+In: None Out: 8 Next State: ((8, 0), 0)
+In: None Out: 16 Next State: ((16, 0), 0)
+In: None Out: 32 Next State: ((32, 0), 0)
+In: None Out: 64 Next State: ((64, 0), 0)
+In: None Out: 128 Next State: ((128, 0), 0)
+In: None Out: 256 Next State: ((256, 0), 0)
+In: None Out: 512 Next State: ((512, 0), 0)
+In: None Out: 1024 Next State: ((1024, 0), 0)
+mul1 results: [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+
+Exercise 4.9:
+Start state: ((2, 2), 0)
+In: None Out: 4 Next State: ((4, 4), 0)
+In: None Out: 16 Next State: ((16, 16), 0)
+In: None Out: 256 Next State: ((256, 256), 0)
+mul2 results: [4, 16, 256]
 """
