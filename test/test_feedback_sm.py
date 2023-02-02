63dd05faafbcfad7be41cdcb3ba802abb8384213
mooracle <yanuar.prakosa@gmail.com>
2023-02-02 18:28:53 +0700

    add FeedbackAdd class and pass test

--- test/test_feedback_sm.py
+++ test/test_feedback_sm.py
@@ -10,9 +10,9 @@ Test page for Feedback combinator
 """
 
 # import from file targeted for testing
-from feedback_sm import Feedback
+from feedback_sm import Feedback, FeedbackAdd
 from cascade import Cascade
-from state_machine import Increment, Delay, Negation, Adder
+from state_machine import Increment, Delay, Negation, Adder, Wire
 from parallel import Parallel
 
 class TestFeedback(unittest.TestCase):
@@ -38,3 +38,17 @@ class TestFeedback(unittest.TestCase):
     def test_fibonacci_feedback(self):
         fib = Feedback(Cascade(Parallel(Delay(1), Cascade(Delay(1), Delay(0))), Adder()))
         self.assertEqual(fib.run(), [1, 2, 3, 5, 8, 13, 21, 34, 55, 89] )
+
+class TestFeedbackAdd(unittest.TestCase):
+    """  
+    test scenario for FeedbackAdd class
+    """
+    def test_transduce_sequence_output_sum_all_inputs(self):
+        """  
+        transduce [0,1,2,3,4,5,6,7,8,9]
+        return [0,0,1,3,6,10,15,21,28,36]
+        """
+        # arrange
+        fa = FeedbackAdd(Delay(0), Wire())
+        # assert
+        self.assertEqual(fa.transduce(range(10)), [0,0,1,3,6,10,15,21,28,36])
