08bb1f3ffd4aa1ce4738e46a83c7ec4c2bc0ec61
mooracle <yanuar.prakosa@gmail.com>
2023-02-09 11:37:02 +0700

    UPDATE: add test for IF class

--- test/test_conditional.py
+++ test/test_conditional.py
@@ -126,3 +126,53 @@ class TestClassMultiplex(unittest.TestCase):
 
         # assert 
         self.assertEqual(capture.getvalue(), expected_print)
+
+class TestClassIf(unittest.TestCase):
+    """  
+    Testing the conditional.py If class
+    """
+    def setUp(self) -> None:
+        self.i = c.If(lambda i: i > 100, sm.Gain(1), sm.Accumulator())
+
+    def test_if_gain_accumulator_set_sm_gain_scenario(self):
+        """  
+        Test If class with SM1: Gain(1) and SM2: Accumulator()
+        Scenrio choose SM1 to run the whole
+        """
+        feeds = [200,3,4,200,300,400,1,200,3]
+        # assert
+        self.assertEqual(self.i.transduce(feeds), [200,3,4,200,300,400,1,200,3])
+
+    def test_if_gain_accumulator_set_accumulaor_scenario(self):
+        """  
+        Test If class with SM1: Gain(1) and SM2: Accumulator()
+        Scenrio choose SM2 to run the whole
+        """
+        feeds = [2,3,4,200,300,400,1,200,3]
+        # assert
+        self.assertEqual(self.i.transduce(feeds), [2,5,9,209,509,909,910,1110,1113])
+
+    def test_if_gain_accumulator_set_accumulaor_verbose_scenario(self):
+        """  
+        Test If class with SM1: Gain(1) and SM2: Accumulator()
+        Scenrio choose SM2 to run the whole
+        """
+        feeds = [2,3,4,200,300,400,1,200,3]
+        capture = io.StringIO()
+        sys.stdout = capture
+        expected_print = \
+            "Start state: ('start', None)\n"+\
+            "In: 2 Out: 2 Next State: ('runningSM2', 2)\n"+\
+            "In: 3 Out: 5 Next State: ('runningSM2', 5)\n"+\
+            "In: 4 Out: 9 Next State: ('runningSM2', 9)\n"+\
+            "In: 200 Out: 209 Next State: ('runningSM2', 209)\n"+\
+            "In: 300 Out: 509 Next State: ('runningSM2', 509)\n"+\
+            "In: 400 Out: 909 Next State: ('runningSM2', 909)\n"+\
+            "In: 1 Out: 910 Next State: ('runningSM2', 910)\n"+\
+            "In: 200 Out: 1110 Next State: ('runningSM2', 1110)\n"+\
+            "In: 3 Out: 1113 Next State: ('runningSM2', 1113)\n"
+        # action
+        self.i.transduce(feeds, verbose=True)
+        sys.stdout = sys.__stdout__
+        # assert
+        self.assertEqual(capture.getvalue(), expected_print)
