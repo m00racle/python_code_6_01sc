56f5cf131dbf8c337cd272ce8dfea98ae717a293
mooracle <yanuar.prakosa@gmail.com>
2023-03-02 21:08:28 +0700

    ADD: __str__ for io.Action class

--- Lec_2_State_Machine/io601.py
+++ Lec_2_State_Machine/io601.py
@@ -15,3 +15,6 @@ class Action:
     def execute(self):
         self.robot.rv = self.rvel
         self.robot.fv = self.fvel
+    
+    def __str__(self) -> str:
+        return '<rv=' + str(float(self.rvel)) + ' rad/s; fv=' + str(float(self.fvel)) + ' m/s>'
