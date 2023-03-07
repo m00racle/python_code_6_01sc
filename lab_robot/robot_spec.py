63b15f5c05a0e3a6617b7fcc44c4e7c50cd38a3d
mooracle <yanuar.prakosa@gmail.com>
2023-03-07 18:34:25 +0700

    TEST: robot rotate odometry vs pose PASS

--- lab_robot/robot_spec.py
+++ lab_robot/robot_spec.py
@@ -6,6 +6,7 @@ this is describe what job the robot is intended to do.
 
 from robot_sm import RobotSM
 import robot_io as io
+from math import pi
 
 # test robot just move forward.
 class TestForward(RobotSM):
@@ -15,3 +16,7 @@ class TestForward(RobotSM):
 class TestRotateForward(RobotSM):
     def getNextValues(self, state, inp, **kwargs) -> tuple:
         return (None, io.Action(self.robot, rvel=-1, fvel=1))
+
+class TestRotate(RobotSM):
+    def getNextValues(self, state, inp, **kwargs) -> tuple:
+        return (None, io.Action(self.robot, rvel=pi/2))
