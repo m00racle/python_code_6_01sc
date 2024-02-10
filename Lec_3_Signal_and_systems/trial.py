50d87f9f018475c0d3bd8905246584f16b536cf2
m00racle <yanuar.prakosa@gmail.com>
2024-02-10 23:45:15 +0700

    initialize test file to import from Lec_2

---
+++ Lec_3_Signal_and_systems/trial.py
@@ -0,0 +1,11 @@
+"""  
+    Special file to test the import statement
+"""
+
+from Lec_2_State_Machine import state_machine as SM 
+
+def test_run():
+    pass
+
+if __name__ == '__main__':
+    test_run()
